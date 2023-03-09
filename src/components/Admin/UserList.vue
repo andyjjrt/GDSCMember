d99d3a9ab1828c94f8d8087051a23dd79314e651
andyjjrt <andyjjrt@gmail.com>
2023-03-09 10:46:40 +0800

    fix(Admin): Change view

---
+++ src/components/Admin/UserList.vue
@@ -0,0 +1,44 @@
+<template>
+  <n-data-table
+    :columns="columns"
+    :data="userData"
+    :pagination="paginationOptions"
+    :summary="createSummary"
+  />
+</template>
+
+<script setup lang="ts">
+import type { DataTableColumns, DataTableCreateSummary } from "naive-ui";
+const props = defineProps({
+  userData: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const columns = [
+  {
+    title: "姓名",
+    key: "name",
+  },
+  {
+    title: "系所",
+    key: "dept",
+  },
+  {
+    title: "Email",
+    key: "email",
+  },
+];
+
+const paginationOptions = { pageSize: 10 };
+
+const createSummary: DataTableCreateSummary = (pageData) => {
+  return {
+    name: {
+      value: h("span", `共 ${props.userData.length} 人`),
+      colSpan: 3,
+    },
+  };
+};
+</script>
