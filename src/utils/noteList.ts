933782837b0bd846ef01ecf3056ecc07f5af6197
andyjjrt <andyjjrt@gmail.com>
2023-03-24 20:55:06 +0800

    feat(note): add 20230322

--- src/utils/noteList.ts
+++ src/utils/noteList.ts
@@ -6,5 +6,9 @@ export const noteList = [
   {
     fileId: "ff2d5fd4-2cbb-4d68-bf52-d23e5a8a9f2c",
     noteId: "20230315"
+  },
+  {
+    fileId: "af55d193-b8ed-4861-acdb-4b98fdb264a2",
+    noteId: "20230322"
   }
 ];
